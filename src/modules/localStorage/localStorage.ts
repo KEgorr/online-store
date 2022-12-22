cb7afe51b6658b8792823da8443fc93f97e3de6c
Svetlana <fidelity-67rus@mail.ru>
2022-12-22 15:53:11 +0300

    feat: add localStorage

---
+++ src/modules/localStorage/localStorage.ts
@@ -0,0 +1,31 @@
+import { ILocalStorage } from '../types/dataTypes';
+
+export default class LocalStorage {
+
+  private readonly storage: ILocalStorage;
+
+  public constructor(getStorage = (): ILocalStorage => window.localStorage) {
+    this.storage = getStorage();
+  }
+
+  public set(key: string, value: string): void  {
+    this.storage.setItem(key, value)
+  }
+
+  public get(key: string): string | null {
+    return this.storage.getItem(key);
+  }
+
+  public getAll() {
+    const data = { ...localStorage };
+    return data;
+  }
+
+  public clear(key: string): void {
+    this.storage.removeItem(key)
+  }
+
+  public clearAll() {
+    localStorage.clear();
+  }
+}
