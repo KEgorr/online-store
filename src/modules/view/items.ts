bf2489a9c47c3c0e9789be3e5ef450b4ae620bc5
Egor Kaeta <kaeta.egor2015@yandex.ru>
2023-01-09 00:21:48 +0300

    feat: adjust massage then no items found

--- src/modules/view/items.ts
+++ src/modules/view/items.ts
@@ -8,46 +8,53 @@ export class Items {
     const itemTemp: HTMLTemplateElement | null = document.querySelector('#itemTemp');
 
     if (itemTemp) {
-      items.forEach((item: IStorageData) => {
-        const itemClone = itemTemp.content.cloneNode(true);
+      if (items.length === 0) {
+        const noItemsText = document.createElement('p');
+        noItemsText.textContent = 'No Items found 😥';
+        noItemsText.classList.add('no-items-text');
+        fragment.append(noItemsText);
+      } else {
+        items.forEach((item: IStorageData) => {
+          const itemClone = itemTemp.content.cloneNode(true);
 
-        if (itemClone instanceof DocumentFragment) {
-          const itemName: HTMLParagraphElement | null = itemClone.querySelector('.item__name');
-          const itemImg: HTMLSpanElement | null = itemClone.querySelector('.item__img');
-          const itemDiscount: HTMLSpanElement | null = itemClone.querySelector('.item__discount');
-          const itemCharStock: HTMLSpanElement | null = itemClone.querySelector('.characteristics__stock');
-          const itemCharBrand: HTMLSpanElement | null = itemClone.querySelector('.characteristics__brand');
-          const itemCharCategory: HTMLSpanElement | null = itemClone.querySelector('.characteristics__category');
-          const itemCharRating: HTMLSpanElement | null = itemClone.querySelector('.characteristics__rating');
-          const curPrice: HTMLParagraphElement | null = itemClone.querySelector('.price__current-price');
-          const origPrice: HTMLSpanElement | null = itemClone.querySelector('.price__original-price');
+          if (itemClone instanceof DocumentFragment) {
+            const itemName: HTMLParagraphElement | null = itemClone.querySelector('.item__name');
+            const itemImg: HTMLSpanElement | null = itemClone.querySelector('.item__img');
+            const itemDiscount: HTMLSpanElement | null = itemClone.querySelector('.item__discount');
+            const itemCharStock: HTMLSpanElement | null = itemClone.querySelector('.characteristics__stock');
+            const itemCharBrand: HTMLSpanElement | null = itemClone.querySelector('.characteristics__brand');
+            const itemCharCategory: HTMLSpanElement | null = itemClone.querySelector('.characteristics__category');
+            const itemCharRating: HTMLSpanElement | null = itemClone.querySelector('.characteristics__rating');
+            const curPrice: HTMLParagraphElement | null = itemClone.querySelector('.price__current-price');
+            const origPrice: HTMLSpanElement | null = itemClone.querySelector('.price__original-price');
 
-          if (
-            itemName &&
-            itemImg &&
-            itemDiscount &&
-            itemCharStock &&
-            itemCharBrand &&
-            itemCharCategory &&
-            itemCharRating &&
-            curPrice &&
-            origPrice
-          ) {
-            itemName.textContent = item.title;
-            itemImg.style.backgroundImage = `url(${item.thumbnail})`;
-            itemDiscount.textContent = `-${item.discountPercentage}%`;
-            itemCharStock.textContent = `${item.stock}`;
-            itemCharBrand.textContent = item.brand;
-            itemCharCategory.textContent = item.category;
-            itemCharRating.textContent = `${item.rating}`;
-            curPrice.textContent = `${item.price}$`;
-            origPrice.textContent = `${(item.price / (1 - item.discountPercentage / 100)).toFixed(0)}$`;
+            if (
+              itemName &&
+              itemImg &&
+              itemDiscount &&
+              itemCharStock &&
+              itemCharBrand &&
+              itemCharCategory &&
+              itemCharRating &&
+              curPrice &&
+              origPrice
+            ) {
+              itemName.textContent = item.title;
+              itemImg.style.backgroundImage = `url(${item.thumbnail})`;
+              itemDiscount.textContent = `-${item.discountPercentage}%`;
+              itemCharStock.textContent = `${item.stock}`;
+              itemCharBrand.textContent = item.brand;
+              itemCharCategory.textContent = item.category;
+              itemCharRating.textContent = `${item.rating}`;
+              curPrice.textContent = `${item.price}$`;
+              origPrice.textContent = `${(item.price / (1 - item.discountPercentage / 100)).toFixed(0)}$`;
 
-            itemClone.querySelector('.item')?.setAttribute('id', `${item.id}`);
-            fragment.append(itemClone);
+              itemClone.querySelector('.item')?.setAttribute('id', `${item.id}`);
+              fragment.append(itemClone);
+            }
           }
-        }
-      });
+        });
+      }
     }
     const itemsSection = document.querySelector('.items-section');
 
